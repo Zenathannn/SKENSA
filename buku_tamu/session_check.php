73258bb502ecd5fa2955be5a5336a4296e09bf9d
Zenathannn <zenathandiancora@gmail.com>
2025-09-03 13:35:46 +0700

    Delete buku_tamu directory

--- buku_tamu/session_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['username'])) {
-    // kalau belum login, lempar balik ke halaman login
-    header("Location: session_login.html");
-    exit;
-}
-?>
